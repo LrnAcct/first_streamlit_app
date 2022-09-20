8465429916010d8d3bc970c2e10ec01a52c859ba
Rohan Shetty <113956380+LrnAcct@users.noreply.github.com>
2022-09-20 08:20:51 +0530

    Update streamlit_app.py
    fixing issue : removing new line between line 6 and 7

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
 streamlit.text ('Kale, Spinach & Rocket Smoothie')
 streamlit.text ('Hard-Boiled Free-Range Egg')
-
 streamlit.header('Breakfast Favorites')
 streamlit.text(' Omega 3 & Blueberry Oatmeal')
 streamlit.text(' (3 Kale, Spinach & Rocket Smoothie'
