e7be082e682a96ed50150ebf38e621556aab4bc4
Rohan Shetty <113956380+LrnAcct@users.noreply.github.com>
2022-09-20 08:19:30 +0530

    Update streamlit_app.py
    Adding new headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
 streamlit.text ('Kale, Spinach & Rocket Smoothie')
 streamlit.text ('Hard-Boiled Free-Range Egg')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
+streamlit.text(' (3 Kale, Spinach & Rocket Smoothie'
+streamlit.text('4 Hard-Boiled Free-Range Egg'
+streamlit.text('OO Avocado Toast')
