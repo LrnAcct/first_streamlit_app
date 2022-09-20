6e67eea37eb094403c81960b0e2639b77906430f
Rohan Shetty <113956380+LrnAcct@users.noreply.github.com>
2022-09-20 08:21:30 +0530

    Update streamlit_app.py
    Commenting code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('My parents had a healthy dinner')
 streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
 streamlit.text ('Kale, Spinach & Rocket Smoothie')
-streamlit.text ('Hard-Boiled Free-Range Egg')
+#streamlit.text ('Hard-Boiled Free-Range Egg')
 streamlit.header('Breakfast Favorites')
 streamlit.text(' Omega 3 & Blueberry Oatmeal')
 streamlit.text(' (3 Kale, Spinach & Rocket Smoothie'
