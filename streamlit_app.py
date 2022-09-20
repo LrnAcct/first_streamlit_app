f75982f96b3437a3c3dd8a4d581625ad595e1bb4
Rohan Shetty <113956380+LrnAcct@users.noreply.github.com>
2022-09-20 08:26:25 +0530

    Update streamlit_app.py
    Fixing issues with script

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,12 @@ streamlit.title('My parents had a healthy dinner')
 streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
 streamlit.text ('Kale, Spinach & Rocket Smoothie')
-#streamlit.text ('Hard-Boiled Free-Range Egg')
+streamlit.text ('Hard-Boiled Free-Range Egg')
+
+
+streamlit.title('My Mom's new healthy dinner')
 streamlit.header('Breakfast Favorites')
-streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit.text(' (3 Kale, Spinach & Rocket Smoothie'
-streamlit.text('4 Hard-Boiled Free-Range Egg'
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('3 Kale, Spinach & Rocket Smoothie')
+streamlit.text('4 Hard-Boiled Free-Range Egg')
 streamlit.text('OO Avocado Toast')
