b94942e4ead9d96fd058689d50cb69a1ae731e75
Rohan Shetty <113956380+LrnAcct@users.noreply.github.com>
2022-09-20 08:41:02 +0530

    Update streamlit_app.py
    Reading from AWS S3 source

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,9 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 OO Avocado Toast')
+
+
+streamlit.title("Build Your Own Fruit Smoothie")
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
