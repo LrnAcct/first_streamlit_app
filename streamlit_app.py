f2d7163d836ef844654869fa9aae56955691ce03
Rohan Shetty <113956380+LrnAcct@users.noreply.github.com>
2022-09-23 07:47:28 +0530

    Update streamlit_app.py
    syntactical error fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 #fruit of choice
 fruit_choice=streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit choice)
+streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response2=requests.get("https://fruityvice.com/api/fruit/" + fruit choice)
+fruityvice_response2=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response2)
